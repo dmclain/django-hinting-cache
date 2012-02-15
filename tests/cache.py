7c4df58cb03c2437f12119954906f6e5f66c6fe0
Dave McLain <github@davemclain.com>
2012-02-14 18:58:34 -0600

    fix some test bugs

--- tests/cache.py
+++ tests/cache.py
@@ -3,6 +3,15 @@ import fudge
 import random
 
 from django_hinting_cache.cache import HintingCache
+from fudge.inspector import arg
+
+def matching(bound):
+    def inner(inner_set):
+        for obj in inner_set:
+            if not obj in bound:
+                return False
+        return True
+    return arg.passes_test(inner)
 
 class HintingCacheTestCase(unittest.TestCase):
 
@@ -20,23 +29,27 @@ class HintingCacheTestCase(unittest.TestCase):
         num = random.randint(2, 7)
         keys = ['key:%i' % random.randint(100,300) for i in range(num)]
         values = ['val:%i' % random.randint(100,300) for i in range(num)]
-        self.inner_cache.expects('get_many').with_args(keys).returns(values)
-        self.assertEqual(values, self.cache.get_many(keys))
+        result = dict(zip(keys, values))
+        self.inner_cache.expects('get_many').with_args(matching(keys)).returns(dict(zip(keys, values)))
+        self.assertEqual(result, self.cache.get_many(keys))
         
     def test_single_hint_get(self):
         hint_key = 'hint_key:%i' % random.randint(100,300)
         key = 'key:%i' % random.randint(100,300)
         value = 'val:%i' % random.randint(100,300)
+        result = {key: value}
         self.cache.hint(hint_key)
-        self.inner_cache.expects('get_many').with_args([hint_key, key]).returns(value)
-        self.assertEqual(value, self.cache.get(key))
+        self.inner_cache.expects('get_many').with_args(matching([hint_key, key])).returns(result)
+        self.assertEqual(result, self.cache.get(key))
         
     def test_single_hint_get_many(self):
+        num = random.randint(2, 7)
         hint_key = 'hint_key:%i' % random.randint(100,300)
         keys = ['key:%i' % random.randint(100,300) for i in range(num)]
         values = ['val:%i' % random.randint(100,300) for i in range(num)]
         self.cache.hint(hint_key)
-        self.inner_cache.expects('get_many').with_args([hint_key] + keys).returns(value)
+        value = dict(zip(keys, values))
+        self.inner_cache.expects('get_many').with_args(matching([hint_key] + keys)).returns(value)
         self.assertEqual(value, self.cache.get_many(keys))
         
     def test_single_hint_get_with_same_key(self):
@@ -69,7 +82,7 @@ class HintingCacheTestCase(unittest.TestCase):
         num = random.randint(2, 6)
         hint_keys = ['hint_key:%i' % random.randint(100,300) for i in range(num)]
         keys = ['key:%i' % random.randint(100,300) for i in range(num)]
-        keys = keys + hint_keys[0]
+        keys = keys + [hint_keys[0]]
         value = 'val:%i' % random.randint(100,300)
         self.cache.hint(*hint_keys)
         self.inner_cache.expects('get_many').with_args(hint_keys + keys).returns(value)
