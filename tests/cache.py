479044ef0946e9981a2228905c0c4bae619f415c
Dave McLain <github@davemclain.com>
2012-02-14 18:39:16 -0600

    test bug

--- tests/cache.py
+++ tests/cache.py
@@ -11,66 +11,66 @@ class HintingCacheTestCase(unittest.TestCase):
         self.cache = HintingCache(self.inner_cache)
 
     def test_no_hint_get(self):
-        key = 'key:%i' % random.randInt(100,300)
-        value = 'val:%i' % random.randInt(100,300)
+        key = 'key:%i' % random.randint(100,300)
+        value = 'val:%i' % random.randint(100,300)
         self.inner_cache.expects('get').with_args(key).returns(value)
         self.assertEqual(value, self.cache.get(key))
 
     def test_no_hint_get_many(self):
-        num = random.randInt(2, 7)
-        keys = ['key:%i' % random.randInt(100,300) for i in range(num)]
-        values = ['val:%i' % random.randInt(100,300) for i in range(num)]
+        num = random.randint(2, 7)
+        keys = ['key:%i' % random.randint(100,300) for i in range(num)]
+        values = ['val:%i' % random.randint(100,300) for i in range(num)]
         self.inner_cache.expects('get_many').with_args(keys).returns(values)
         self.assertEqual(values, self.cache.get_many(keys))
         
     def test_single_hint_get(self):
-        hint_key = 'hint_key:%i' % random.randInt(100,300)
-        key = 'key:%i' % random.randInt(100,300)
-        value = 'val:%i' % random.randInt(100,300)
+        hint_key = 'hint_key:%i' % random.randint(100,300)
+        key = 'key:%i' % random.randint(100,300)
+        value = 'val:%i' % random.randint(100,300)
         self.cache.hint(hint_key)
         self.inner_cache.expects('get_many').with_args([hint_key, key]).returns(value)
         self.assertEqual(value, self.cache.get(key))
         
     def test_single_hint_get_many(self):
-        hint_key = 'hint_key:%i' % random.randInt(100,300)
-        keys = ['key:%i' % random.randInt(100,300) for i in range(num)]
-        values = ['val:%i' % random.randInt(100,300) for i in range(num)]
+        hint_key = 'hint_key:%i' % random.randint(100,300)
+        keys = ['key:%i' % random.randint(100,300) for i in range(num)]
+        values = ['val:%i' % random.randint(100,300) for i in range(num)]
         self.cache.hint(hint_key)
         self.inner_cache.expects('get_many').with_args([hint_key] + keys).returns(value)
         self.assertEqual(value, self.cache.get_many(keys))
         
     def test_single_hint_get_with_same_key(self):
-        hint_key = 'hint_key:%i' % random.randInt(100,300)
+        hint_key = 'hint_key:%i' % random.randint(100,300)
         key = hint_key
-        value = 'val:%i' % random.randInt(100,300)
+        value = 'val:%i' % random.randint(100,300)
         self.cache.hint(hint_key)
         self.inner_cache.expects('get').with_args(key).returns(value)
         self.assertEqual(value, self.cache.get(key))
         
     def test_multi_hint_get(self):
-        num = random.randInt(2, 6)
-        hint_keys = ['hint_key:%i' % random.randInt(100,300) for i in range(num)]
-        key = 'key:%i' % random.randInt(100,300)
-        value = 'val:%i' % random.randInt(100,300)
+        num = random.randint(2, 6)
+        hint_keys = ['hint_key:%i' % random.randint(100,300) for i in range(num)]
+        key = 'key:%i' % random.randint(100,300)
+        value = 'val:%i' % random.randint(100,300)
         self.cache.hint(*hint_keys)
         self.inner_cache.expects('get_many').with_args(hint_keys + [key]).returns(value)
         self.assertEqual(value, self.cache.get(key))
         
     def test_multi_hint_get_many(self):
-        num = random.randInt(2, 6)
-        hint_keys = ['hint_key:%i' % random.randInt(100,300) for i in range(num)]
-        keys = ['key:%i' % random.randInt(100,300) for i in range(num)]
-        value = 'val:%i' % random.randInt(100,300)
+        num = random.randint(2, 6)
+        hint_keys = ['hint_key:%i' % random.randint(100,300) for i in range(num)]
+        keys = ['key:%i' % random.randint(100,300) for i in range(num)]
+        value = 'val:%i' % random.randint(100,300)
         self.cache.hint(*hint_keys)
         self.inner_cache.expects('get_many').with_args(hint_keys + keys).returns(value)
         self.assertEqual(value, self.cache.get_many(keys))
         
     def test_multi_hint_get_with_same_key(self):
-        num = random.randInt(2, 6)
-        hint_keys = ['hint_key:%i' % random.randInt(100,300) for i in range(num)]
-        keys = ['key:%i' % random.randInt(100,300) for i in range(num)]
+        num = random.randint(2, 6)
+        hint_keys = ['hint_key:%i' % random.randint(100,300) for i in range(num)]
+        keys = ['key:%i' % random.randint(100,300) for i in range(num)]
         keys = keys + hint_keys[0]
-        value = 'val:%i' % random.randInt(100,300)
+        value = 'val:%i' % random.randint(100,300)
         self.cache.hint(*hint_keys)
         self.inner_cache.expects('get_many').with_args(hint_keys + keys).returns(value)
         self.assertEqual(value, self.cache.get_many(keys))
